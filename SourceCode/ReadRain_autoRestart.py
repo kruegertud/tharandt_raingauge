000382d2e76b0a0ca1631b25167ec089606c6e20
kruegertud <74175639+kruegertud@users.noreply.github.com>
2024-04-04 10:15:31 +0200

    Update ReadRain_autoRestart.py
    removed tilt detection

--- SourceCode/ReadRain_autoRestart.py
+++ SourceCode/ReadRain_autoRestart.py
@@ -1,5 +1,6 @@
 # ReadRain.py
 # PythonScript which saves minutly PrecipationData from a connected Davs Rain Gauge to an sql lite database
+# script is auto shutdown on minute 20min57s each hour - so has to be restarted at minute 21 via crontab - 3s of blind spot each hour.
 # Robert Krüger - TUD
 
 import RPi.GPIO as GPIO
@@ -14,9 +15,6 @@ count = 0
 GPIO.setmode(GPIO.BOARD)
 # Sensor Pin noch zu setzen
 GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-
 
 def itsraining(channel):
     global count
@@ -50,18 +48,7 @@ try:
 
             #read time and count for last minute -> save to db
             now = datetime.utcnow().replace(second = 0, microsecond = 0)
-            #read tiltstate
-            if now.minute == 0:
-                conn = sqlite3.connect('/home/pi/Documents/database/sht31.db')
-                c = conn.cursor()
-                if GPIO.input(12) == True:
-                    tilt = 1
-                else:
-                    tilt = 0
-                print("INSERT INTO data VALUES ('K', " + str(tilt) + ', "' + str(now) + '", 0)')
-                c.execute("INSERT INTO data VALUES ('K', " + str(tilt) + ', "' + str(now) + '", 0)')
-                conn.commit()
-            
+                        
             conn = sqlite3.connect('/home/pi/Documents/database/Rain.db')
             c = conn.cursor()
             
@@ -71,8 +58,7 @@ try:
             conn.commit()
             #reset counter
             startup = 0
-            count = 0
-            
+            count = 0            
 
             #sleep for 57 seconds
             sleeptime = ((now + timedelta(seconds = 57)) - datetime.utcnow()).total_seconds()
